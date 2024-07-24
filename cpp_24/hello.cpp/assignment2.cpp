13c07413b445af60a54fdd5033f8778afa629db2
rmar0 <164451746+rmar0@users.noreply.github.com>
2024-07-23 22:58:23 -0700

    Add files via upload

---
+++ cpp_24/hello.cpp/assignment2.cpp
@@ -0,0 +1,51 @@
+/*
+Name: Ruth Marroquin, 5008310315#, 1003, Assignment #2
+Description: 
+Input: 
+Output: 
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+//initialize side length
+double side_length = 0.0;
+
+//prompt
+cout << "Enter octagon’s side length: ";
+double input = 0.0;
+cin >> side_length;
+
+// function to calculate the area of an octagon
+double area = 2 * pow(side_length, 2) * (1 + sqrt(2));
+
+//calculate the circumference
+ double circumcircle_radius = (0.5 * side_length) * sqrt(4 + 2 * sqrt(2));
+
+ //calculate circle radius
+ double incircle_radius = (0.5 * side_length) * (1 + sqrt(2));
+
+//calculation table
+cout << "+--------------+--------------+--------------+--------------+\n";
+//fixed point
+cout << fixed << setprecision(2);
+cout << "| side length  | area         | circumcircle | incircle     |\n";
+cout << "+--------------+--------------+--------------+--------------+\n";
+cout << "| " << setw(12) << side_length 
+              << " | " << setw(12) << area 
+              << " | " << setw(12) << circumcircle_radius 
+              << " | " << setw(12) << incircle_radius 
+              << " |\n";
+    cout << "+--------------+--------------+--------------+--------------+\n";
+
+
+    return 0;
+
+
+
+}
